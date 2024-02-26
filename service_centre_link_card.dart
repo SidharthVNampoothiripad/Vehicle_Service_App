6025c79b22dc2b7d53e53bd725f75013d70dbbf0
VISHAL V NAIR <90333863+vishalvijaynair@users.noreply.github.com>
2024-02-27 02:46:41 +0530

    Create service_centre_link_card.dart

---
+++ service_centre_link_card.dart
@@ -0,0 +1,53 @@
+// service_center_link_card.dart
+import 'package:flutter/material.dart';
+import 'carousel_detailed.dart'; // Import the necessary file
+
+class ServiceCenterLinkCard extends StatelessWidget {
+  final String serviceCenterName;
+
+  ServiceCenterLinkCard({required this.serviceCenterName});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // Navigate to the detailed page when the card is clicked
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => CarouselDetailed(selectedService: serviceCenterName),
+          ),
+        );
+      },
+      child: Card(
+        elevation: 5.0,
+        margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              alignment: Alignment.topRight,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(8.0),
+                  padding: EdgeInsets.all(6.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Text(
+                    serviceCenterName,
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
