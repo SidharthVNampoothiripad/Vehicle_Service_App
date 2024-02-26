4069545958889da7f591726b2fbc3241a8dba84a
VISHAL V NAIR <90333863+vishalvijaynair@users.noreply.github.com>
2024-02-27 02:34:55 +0530

    Update home_screen.dart

--- home_screen.dart
+++ home_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'service_center_card.dart';
 import 'authentication_page.dart';
 import 'search_page.dart'; 
-import 'carousel.dart'; // Import the CustomCarousel widget
+import 'carousel.dart';
 
 class CarServiceHomePage extends StatefulWidget {
   @override
@@ -46,7 +46,6 @@ class _CarServiceHomePageState extends State<CarServiceHomePage> {
       ),
       body: ListView(
         children: <Widget>[
-          // Add the CustomCarousel widget
           CustomCarousel(),
           
           Padding(
@@ -58,33 +57,38 @@ class _CarServiceHomePageState extends State<CarServiceHomePage> {
           ),
           ServiceCenterCard(
             name: 'Service Center A',
+            location: 'Chengannur',
             imageUrl: 'https://plus.unsplash.com/premium_photo-1677009541899-28700f6c20a8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y2FyJTIwd29ya3Nob3B8ZW58MHx8MHx8fDA%3D',
             rating: 4.5,
-            services: ['Oil Change', 'Tire Rotation'],
+            services: ['Wheel alignment', 'Tire Rotation','Nitrogen filling'],
           ),
-           ServiceCenterCard(
+          ServiceCenterCard(
             name: 'Service Center B',
+            location: 'Thiruvalla',
             imageUrl: 'https://plus.unsplash.com/premium_photo-1676998430827-aee8e597b013?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGNhciUyMHdvcmtzaG9wfGVufDB8fDB8fHww',
             rating: 4.2,
-            services: ['Brake Inspection', 'Engine Tune-up'],
+            services: ['Oil change', 'Engine Tune-up','Gear box and Clutch maintenance'],
           ),
           ServiceCenterCard(
             name: 'Service Center C',
+            location: 'Changanassery',
             imageUrl: 'https://plus.unsplash.com/premium_photo-1663045610933-af6f1ca26c6e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGNhciUyMHdvcmtzaG9wfGVufDB8fDB8fHww',
             rating: 4.2,
-            services: ['Brake Inspection', 'Engine Tune-up'],
+            services: ['Dent and scratch repair', 'Washing','Graphite coating'],
           ),
           ServiceCenterCard(
             name: 'Service Center D',
+            location: 'Pandalam',
             imageUrl: 'https://images.unsplash.com/photo-1615906655593-ad0386982a0f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FyJTIwd29ya3Nob3B8ZW58MHx8MHx8fDA%3D',
-            rating: 4.2,
-            services: ['Brake Inspection', 'Engine Tune-up'],
+            rating: 4.0,
+            services: ['Brake Inspection', 'Engine Tune-Up','Chassis'],
           ),
           ServiceCenterCard(
             name: 'Service Center E',
+            location: 'Adoor',
             imageUrl: 'https://plus.unsplash.com/premium_photo-1675810094948-d4634e766d2b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y2FyJTIwc2VydmljZXxlbnwwfHwwfHx8MA%3D%3D',
-            rating: 4.2,
-            services: ['Brake Inspection', 'Engine Tune-up'],
+            rating: 3.9,
+            services: ['Car detailing','Glasses','Washing'],
           ),
           // Add more ServiceCenterCard widgets for other service centers
         ],
@@ -92,4 +96,3 @@ class _CarServiceHomePageState extends State<CarServiceHomePage> {
     );
   }
 }
-      
