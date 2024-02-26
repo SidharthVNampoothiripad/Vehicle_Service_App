7e1e9f7cb372a10f8edf6ebe0100fdbce4322225
VISHAL V NAIR <90333863+vishalvijaynair@users.noreply.github.com>
2024-02-26 13:56:44 +0530

    Update signup_page.dart

---
+++ signup_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign Up'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+            SizedBox(height: 12.0),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                // Perform signup logic
+                // For example, you can use Firebase Authentication
+
+                // For demonstration purposes, we will print the email and password
+                print('Email: ${_emailController.text}');
+                print('Password: ${_passwordController.text}');
+
+                // Handle successful signup, e.g., navigate to home page
+                Navigator.pop(context);
+              },
+              child: Text('Sign Up'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
