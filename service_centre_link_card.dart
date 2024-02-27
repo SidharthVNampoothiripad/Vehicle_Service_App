39e1bfb81c2dcb14432acad31d0cddb8edeb3385
VISHAL V NAIR <90333863+vishalvijaynair@users.noreply.github.com>
2024-02-27 12:48:19 +0530

    Update service_centre_link_card.dart

--- service_centre_link_card.dart
+++ service_centre_link_card.dart
@@ -22,30 +22,20 @@ class ServiceCenterLinkCard extends StatelessWidget {
       child: Card(
         elevation: 5.0,
         margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Stack(
-              alignment: Alignment.topRight,
-              children: [
-                Container(
-                  margin: EdgeInsets.all(8.0),
-                  padding: EdgeInsets.all(6.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: Text(
-                    serviceCenterName,
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ],
+        child: Container(
+          margin: EdgeInsets.all(8.0),
+          padding: EdgeInsets.all(6.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: Text(
+            serviceCenterName,
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
             ),
-          ],
+          ),
         ),
       ),
     );
