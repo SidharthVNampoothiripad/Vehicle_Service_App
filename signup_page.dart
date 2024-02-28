40ac0f6847c38bfd2f5c5fb81f0dcc6afca1beb5
VISHAL V NAIR <90333863+vishalvijaynair@users.noreply.github.com>
2024-02-29 03:10:35 +0530

    Update signup_page.dart

--- signup_page.dart
+++ signup_page.dart
@@ -22,124 +22,129 @@ class _SignUpPageState extends State<SignUpPage> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                controller: _nameController,
-                decoration: InputDecoration(
-                  labelText: 'Name',
+          child: Form(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: _nameController,
+                  decoration: InputDecoration(
+                    labelText: 'Name',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Name is required';
+                    } else if (!RegExp(r'^[a-zA-Z ]+$').hasMatch(value)) {
+                      return 'Name should not contain numbers';
+                    }
+                    return null;
+                  },
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
                 ),
-              ),
-              SizedBox(height: 12.0),
-              TextField(
-                controller: _phoneNumberController,
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(
-                  labelText: 'Phone Number',
+                SizedBox(height: 12.0),
+                TextFormField(
+                  controller: _phoneNumberController,
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                    labelText: 'Phone Number',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Phone number is required';
+                    } else if (value.length != 10 || int.tryParse(value) == null) {
+                      return 'Phone number should have only 10 digits';
+                    }
+                    return null;
+                  },
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
                 ),
-              ),
-              SizedBox(height: 12.0),
-              TextField(
-                controller: _locationController,
-                decoration: InputDecoration(
-                  labelText: 'Location',
+                SizedBox(height: 12.0),
+                TextFormField(
+                  controller: _locationController,
+                  decoration: InputDecoration(
+                    labelText: 'Location',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Location is required';
+                    }
+                    return null;
+                  },
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
                 ),
-              ),
-              SizedBox(height: 12.0),
-              TextField(
-                controller: _usernameController,
-                decoration: InputDecoration(
-                  labelText: 'Username',
+                SizedBox(height: 12.0),
+                TextFormField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Username is required';
+                    }
+                    return null;
+                  },
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
                 ),
-              ),
-              SizedBox(height: 12.0),
-              TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
+                SizedBox(height: 12.0),
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Password is required';
+                    } else if (value.length < 8) {
+                      return 'Password must be at least 8 characters';
+                    }
+                    return null;
+                  },
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
                 ),
-              ),
-              SizedBox(height: 12.0),
-              TextField(
-                controller: _confirmPasswordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Confirm Password',
+                SizedBox(height: 12.0),
+                TextFormField(
+                  controller: _confirmPasswordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Confirm Password',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Confirm Password is required';
+                    } else if (value != _passwordController.text) {
+                      return 'Passwords do not match';
+                    }
+                    return null;
+                  },
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
                 ),
-              ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () {
-                  if (_validateForm()) {
-                    // Perform signup logic
-                    // For example, you can use Firebase Authentication
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // Check if the form is valid before proceeding
+                    if (Form.of(context).validate()) {
+                      // Perform signup logic
+                      // For demonstration purposes, we will print the form data
+                      print('Name: ${_nameController.text}');
+                      print('Phone Number: ${_phoneNumberController.text}');
+                      print('Location: ${_locationController.text}');
+                      print('Username: ${_usernameController.text}');
+                      print('Password: ${_passwordController.text}');
+                      print('Confirm Password: ${_confirmPasswordController.text}');
 
-                    // For demonstration purposes, we will print the form data
-                    print('Name: ${_nameController.text}');
-                    print('Phone Number: ${_phoneNumberController.text}');
-                    print('Location: ${_locationController.text}');
-                    print('Username: ${_usernameController.text}');
-                    print('Password: ${_passwordController.text}');
-                    print('Confirm Password: ${_confirmPasswordController.text}');
-
-                    // Handle successful signup, e.g., navigate to home page
-                    Navigator.pop(context);
-                  }
-                },
-                child: Text('Submit'),
-              ),
-            ],
+                      // Handle successful signup, e.g., navigate to home page
+                      Navigator.pop(context);
+                    }
+                  },
+                  child: Text('Submit'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
-    );
-  }
-
-  bool _validateForm() {
-    if (_nameController.text.isEmpty ||
-        _phoneNumberController.text.isEmpty ||
-        _locationController.text.isEmpty ||
-        _usernameController.text.isEmpty ||
-        _passwordController.text.isEmpty ||
-        _confirmPasswordController.text.isEmpty) {
-      _showErrorDialog('All fields are required');
-      return false;
-    } else if (!RegExp(r'^[a-zA-Z ]+$').hasMatch(_nameController.text)) {
-      _showErrorDialog('Name should not contain numbers');
-      return false;
-    } else if (_phoneNumberController.text.length != 10 ||
-        int.tryParse(_phoneNumberController.text) == null) {
-      _showErrorDialog('Phone number should have only 10 digits');
-      return false;
-    } else if (_passwordController.text != _confirmPasswordController.text) {
-      _showErrorDialog('Passwords do not match');
-      return false;
-    }
-
-    // Add more specific validation if needed
-
-    return true;
-  }
-
-  void _showErrorDialog(String message) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Error'),
-          content: Text(message),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('OK'),
-            ),
-          ],
-        );
-      },
     );
   }
 }
