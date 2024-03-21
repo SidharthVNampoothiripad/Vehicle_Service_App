26513a7d95872d1b61fc4a3f0f3ddd2f1e197fd7
VISHAL V NAIR <90333863+vishalvijaynair@users.noreply.github.com>
2024-03-21 14:29:42 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
-import 'home_screen.dart';
+//import 'home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'login_page.dart';
+void main()  async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+       apiKey: "AIzaSyB0stCkFLyNFJI20FmE0zaFzTP2KBPEY70",
+        appId: "1:354057994820:android:931f4b3af4c6011154b211",
+        messagingSenderId: "354057994820",
+        projectId: "fixflow-e3983",
+        storageBucket: "fixflow-e3983.appspot.com"),
+  );
 
-void main() => runApp(CarServiceApp());
+  runApp(CarServiceApp());
+}
+
 
 class CarServiceApp extends StatelessWidget {
   // Simulate some initialization process
@@ -20,13 +34,13 @@ class CarServiceApp extends StatelessWidget {
             debugShowCheckedModeBanner: false,
             title: 'Car Service App',
             theme: ThemeData(
-              primarySwatch: Colors.blue,
+              primarySwatch: Colors.yellow,
               colorScheme: ColorScheme.fromSwatch(
                 accentColor: Colors.amber,
               ), // Accent color directly under ThemeData
               fontFamily: 'Montserrat', // Your selected font
             ),
-            home: CarServiceHomePage(),
+            home: LoginPage(),
           );
         } else {
           // Show a loading indicator while waiting for initialization to complete
@@ -34,11 +48,11 @@ class CarServiceApp extends StatelessWidget {
   debugShowCheckedModeBanner: false,
   home: Scaffold(
     body: Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: NetworkImage(
-            'https://images.unsplash.com/photo-1595521488367-9b130f86bbe3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNhciUyMGltYWdlJTIwcG90cmFpdHxlbnwwfDF8MHx8fDA%3D', // Replace with your actual image URL
-          ),
+  decoration: BoxDecoration(
+    image: DecorationImage(
+      image: AssetImage(
+        "assets/loading_page.png",
+      ),
           fit: BoxFit.cover,
         ),
       ),
