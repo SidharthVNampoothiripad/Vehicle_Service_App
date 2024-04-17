d376e39807f64a26fd5cc2f9e846c16dec5c6d3b
VISHAL V NAIR <90333863+vishalvijaynair@users.noreply.github.com>
2024-04-18 01:20:57 +0530

    Update orders_completed.dart

--- lib/Orders/orders_completed.dart
+++ lib/Orders/orders_completed.dart
@@ -1,78 +1,76 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hellogram/Orders/RatingSubmissionPage.dart';
 
 class OrdersCompleted extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My Orders'),
-        backgroundColor: Color.fromARGB(255, 182, 200, 247),
-      ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance
-            .collection('orders_completed')
-            .doc(FirebaseAuth.instance.currentUser!.email)
-            .collection('orders')
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
+  appBar: AppBar(
+    title: Text('Completed Orders'),
+    backgroundColor: Color.fromARGB(255, 207, 173, 210),
+  ),
+  body: StreamBuilder<QuerySnapshot>(
+    stream: FirebaseFirestore.instance
+        .collection('orders_completed')
+        .doc(FirebaseAuth.instance.currentUser!.email)
+        .collection('orders')
+        .snapshots(),
+    builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return Center(child: CircularProgressIndicator());
+      }
 
-          if (snapshot.hasError) {
-            print('Error retrieving orders: ${snapshot.error}');
-            return Center(child: Text('Error: ${snapshot.error}'));
-          }
+      if (snapshot.hasError) {
+        print('Error retrieving orders: ${snapshot.error}');
+        return Center(child: Text('Error: ${snapshot.error}'));
+      }
 
-          final orders = snapshot.data!.docs;
+      final orders = snapshot.data!.docs;
 
-          if (orders.isEmpty) {
-            return Center(
-              child: Text(
-                'You have no Completed Orders',
-                style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-              ),
-            );
-          }
+      if (orders.isEmpty) {
+        return Center(
+          child: Text(
+            'You have no Completed Orders',
+            style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+          ),
+        );
+      }
 
-          return ListView.builder(
-            itemCount: orders.length,
-            itemBuilder: (context, index) {
-              final orderData = orders[index].data() as Map<String, dynamic>;
+      return ListView.builder(
+        itemCount: orders.length,
+        itemBuilder: (context, index) {
+          final orderData = orders[index].data() as Map<String, dynamic>;
 
-              String orderedDate = orderData['orderedDate'] ?? 'N/A';
-              String orderedTime = orderData['orderedTime'] ?? 'N/A';
-              String totalAmount = orderData['totalAmount']?.toString() ?? 'N/A';
-              List<dynamic>? selectedServicesList = orderData['selectedServices'];
-              String selectedServices = selectedServicesList?.join(', ') ?? 'N/A';
-              String userEmail = orders[index].id;
+          String orderedDate = orderData['orderedDate'] ?? 'N/A';
+          String orderedTime = orderData['orderedTime'] ?? 'N/A';
+          String totalAmount = orderData['totalAmount']?.toString() ?? 'N/A';
+          List<dynamic>? selectedServicesList = orderData['selectedServices'];
+          String selectedServices = selectedServicesList?.join(', ') ?? 'N/A';
+          String userEmail = orders[index].id;
 
-              return FutureBuilder<DocumentSnapshot>(
-                future: FirebaseFirestore.instance
-                    .collection('Service_Centres')
-                    .doc(orders[index].id)
-                    .get(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    // If still waiting for data, display a loading indicator or placeholder
-                    return CircularProgressIndicator();
-                  }
-                  if (snapshot.hasError) {
-                    // If an error occurs while fetching data, handle it here
-                    return Text('Error: ${snapshot.error}');
-                  }
-                  if (!snapshot.hasData || !snapshot.data!.exists) {
-                    // If document doesn't exist or no data found, display appropriate message
-                    return Text('Service Center not found');
-                  }
+          return FutureBuilder<DocumentSnapshot>(
+            future: FirebaseFirestore.instance
+                .collection('Service_Centres')
+                .doc(orders[index].id)
+                .get(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              }
+              if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              }
+              if (!snapshot.hasData || !snapshot.data!.exists) {
+                return Text('Service Center not found');
+              }
 
-                  // Extract the service center name from the snapshot data
-                  String serviceCenterName = snapshot.data!.get('Service Center Name');
+              String serviceCenterName = snapshot.data!.get('Service Center Name');
 
-                  // Now you can return the ListTile with the correct service center name
-                  return ListTile(
+              return Column(
+                children: [
+                  ListTile(
                     title: Text(
                       'Order ${index + 1}',
                       style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
@@ -81,7 +79,7 @@ class OrdersCompleted extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          'Service Center Name: $serviceCenterName', // Display service center name
+                          'Service Center Name: $serviceCenterName',
                           style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                         ),
                         Text(
@@ -106,13 +104,40 @@ class OrdersCompleted extends StatelessWidget {
                         ),
                       ],
                     ),
-                  );
-                },
+                    trailing: SizedBox(
+                      width: 150,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RatingSubmissionPage(userEmail: userEmail),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.symmetric(vertical: 16),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                           backgroundColor: Color.fromARGB(255, 25, 87, 46).withOpacity(0.9),
+                        ),
+                        child: Text(
+                          'Rate us',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Divider(),
+                ],
               );
             },
           );
         },
-      ),
-    );
+      );
+    },
+  ),
+);
   }
 }
